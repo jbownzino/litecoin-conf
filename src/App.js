d4015c6b14ca1d9bfa1e63f549d0c75d9d46ccdc
Jared Bowns <soundboyzent@gmail.com>
2018-03-22 13:09:53 -0400

    landing page complete, little keyframe in there

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react';
-import logo from './assets/images/litecoin-logo.png';
-import pd from './assets/images/pedestal.png';
+import logo from './assets/litecoin-logo.png';
+import text from './assets/litecoin-text.png';
 import './App.css';
 
 class LandingContent extends React.Component {
     render() {
         return (
             <div className="content">
-                <img className="logo drop-shadow" src={logo} alt="Logo" />
-                <div className="details jump">
-                    <img className="pd drop-shadow" src={pd} alt="Pedestal" />
-                    <h1>Litecoin Global Payments Conference</h1>
-                    <h2>Presented by the Litecoin Foundation</h2>
-                    <h3>South San Francisco Conference Center | September 14-15 2018</h3>
+                <img className="logo jump drop-shadow" src={logo} alt="Logo" />
+                <div className="details">
+                    <img className="text" src={text} alt="Logo" />
+                    <h1>Global Payments Conference</h1>
+                    <h3>South San Francisco Conference Center | September 14-15, 2018</h3>
                 </div>
             </div>
         )
