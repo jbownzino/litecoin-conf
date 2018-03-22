fd566130efeaa10d542ddd688b7d9cde49b1e275
Jared Bowns <soundboyzent@gmail.com>
2018-03-22 12:50:16 -0400

    updating npm

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './assets/images/litecoin-logo.png';
+import pd from './assets/images/pedestal.png';
 import './App.css';
 
 class LandingContent extends React.Component {
     render() {
         return (
             <div className="content">
-                <h1>Global Payments Conference</h1>
+                <img className="logo drop-shadow" src={logo} alt="Logo" />
+                <div className="details jump">
+                    <img className="pd drop-shadow" src={pd} alt="Pedestal" />
+                    <h1>Litecoin Global Payments Conference</h1>
+                    <h2>Presented by the Litecoin Foundation</h2>
+                    <h3>South San Francisco Conference Center | September 14-15 2018</h3>
+                </div>
             </div>
         )
     }
@@ -15,12 +22,12 @@ class LandingContent extends React.Component {
 class App extends Component {
   render() {
     return (
-      <div className="flex">
-          <div className="overlay" />
+      <div className="image-bg">
         <LandingContent />
       </div>
     );
   }
 }
+/*<img src={pd} alt="Logo" />*/
 
 export default App;
