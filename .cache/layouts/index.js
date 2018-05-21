54a0aef6d274c5a55b907ed80b07afe0fd0989bd
harrythedev <harrythedev@gmail.com>
2018-05-21 03:17:35 -0400

    remove .cache and .vscode

--- .cache/layouts/index.js
+++
@@ -1,7 +0,0 @@
-
-  import React from "react"
-  import Component from "/Users/harryhur/Desktop/codes/litecoinsummit.org/src/layouts/index.js"
-  import data from "/Users/harryhur/Desktop/codes/litecoinsummit.org/.cache/json/layout-index.json"
-
-  export default (props) => <Component {...props} {...data} />
-  
