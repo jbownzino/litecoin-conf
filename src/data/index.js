5cd2e8bcb7fbfabbe999a05709b8a50b691aeee3
harrythedev <harrythedev@gmail.com>
2018-06-03 19:05:45 -0400

    change ticket price

--- src/data/index.js
+++ src/data/index.js
@@ -227,13 +227,13 @@ export const tickets = [
     title: "Summit Day 1 + 2",
     img: "",
     content: ["Access to both days of the summit", "Free Lanyard on arrival", "*Non Refundable"],
-    price: 400
+    price: 490
   },
   {
     title: "Summit Day 2",
     img: "",
     content: ["Access to summit on 15th Sep Only", "Free Lanyard on arrival", "*Non Refundable"],
-    price: 40
+    price: 49
   }
 ]
 
